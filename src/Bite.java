15e716590d2db861c5db7af583a5b03d845d5c6f
Ricky He <rickyhe@my.bridgeport.edu>
2019-10-06 19:32:09 -0400

    Type of the List

--- src/Bite.java
+++ src/Bite.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Bite {
     //Bite data
     private int SIZE = 25000;
-    private ArrayList work = new ArrayList(SIZE);
+    private ArrayList<Integer> work = new ArrayList(SIZE);
     private int totalBites;
 
     //Creates a Bite object with a List that starts with the number given to it (x) till 25000 after it
