757a78167733ba61b79d32c3897659cab30fbaaf
Ricky He <rickyhe@my.bridgeport.edu>
2019-10-06 19:13:06 -0400

    Bite class

--- src/Bite.java
+++ src/Bite.java
@@ -1,13 +1,20 @@
 import java.util.ArrayList;
 
 public class Bite {
-    //Creates a Bite object with 25000 numbers in it
+    //Bite data
     private int SIZE = 25000;
     private ArrayList work = new ArrayList(SIZE);
 
-
+    //Creates a Bite object with a List that starts with the number given to it (x) till 25000 after it
     public Bite(int x){
-
+        for (int i = x; i < x + SIZE - 1 ; i++) {
+            work.add(i);
+        }
+    }
+    //Creates and return a Bite interval
+    public Bite getWork(int x){
+        Bite workBite = new Bite(x);
+        return workBite;
     }
 
 
