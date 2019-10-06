7b87b3bbfce6c081d00871355832561a11b65c97
Ricky He <rickyhe@my.bridgeport.edu>
2019-10-06 18:50:28 -0400

    Prime number method

---
+++ src/Bite.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Bite {
+    //Creates a Bite object with 25000 numbers in it
+    private int SIZE = 25000;
+    private ArrayList work = new ArrayList(SIZE);
+
+
+    public Bite(int x){
+
+    }
+
+
+}
