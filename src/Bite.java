1b3eeaa131ad7431ff803dc1a88414dd89c41fb8
Ricky He <rickyhe@my.bridgeport.edu>
2019-10-06 20:03:02 -0400

    Dynamic Bite

--- src/Bite.java
+++ src/Bite.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 
 public class Bite {
     //Bite data
-    private int SIZE = 25000;
-    private ArrayList<Integer> work = new ArrayList(SIZE);
+    private ArrayList<Integer> work;
     private int totalBites;
 
-    //Creates a Bite object with a List that starts with the number given to it (x) till 25000 after it
-    public Bite(int x){
-        for (int i = x; i < x + SIZE - 1 ; i++) {
+    //Creates a Bite object with a List that starts with the number given to it (start_point) till 25000 after it
+    public Bite(int size,int start_point){
+        work = new ArrayList<>(size);
+        for (int i = start_point; i < start_point + size - 1 ; i++) {
             work.add(i);
         }
         totalBites++;
     }
     //Creates and return a Bite interval
-    public Bite getWork(int x){
-        Bite workBite = new Bite(x);
+    public Bite getWork(int size,int start_point){
+        Bite workBite = new Bite(size,start_point);
         return workBite;
     }
     //Keeps track of how many bites are made
